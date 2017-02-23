efc4c604250447757ed177544d5773fb827be5ee
lukedevour <luke.boyd@umontana.edu>
2017-02-23 16:22:38 -0700

    added the readme and cleaned up my JS code

---
+++ HW4/readme.md
@@ -0,0 +1,9 @@
+# HW 4
+
+## Work Progress
+
+This week went fine with regard to the actual coding assignment.
+
+## Weekly Issues
+
+One issue encountered this week, had to do with my github worlflow...
--- HW4/sketch.js
+++ HW4/sketch.js
@@ -2,33 +2,31 @@ var ellipseX = 0;
 
 // setup
 function setup() {
-  createCanvas(600, 500);
+    createCanvas(600, 500);
 }
 
 function draw() {
-// background
-background(0);
+    // background
+    background(0);
 
 
+    // target
+    // Concentric Circles
+    ellipse(ellipseX, 200, 80, 80);
+    ellipse(ellipseX, 200, 70, 70);
+    ellipse(ellipseX, 200, 60, 60);
+    ellipse(ellipseX, 200, 50, 50);
+    ellipse(ellipseX, 200, 40, 40);
+    ellipse(ellipseX, 200, 30, 30);
 
-// target
-// Concentric Circles
-ellipse(ellipseX, 200, 80, 80);
-ellipse(ellipseX, 200, 70, 70);
-ellipse(ellipseX, 200, 60, 60);
-ellipse(ellipseX, 200, 50, 50);
-ellipse(ellipseX, 200, 40, 40);
-ellipse(ellipseX, 200, 30, 30);
 
+    // lateral movement of big ellipse
+    ellipseX = ellipseX + 1;
 
-// lateral movement of big ellipse
-ellipseX = ellipseX + 1;
+    // movement of small ellipse with mouse
+    ellipse(mouseY, 25, 25, 25);
 
-// movement of small ellipse with mouse
-ellipse(mouseY, 25, 25, 25);
-
-// ellipse for mouse
-ellipse(mouseX, mouseY, 35, 35);
-
+    // ellipse for mouse
+    ellipse(mouseX, mouseY, 35, 35);
 
 }
