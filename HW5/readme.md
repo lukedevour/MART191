49171c9f27a4f10d42a67287dbdea7d606645c5a
lukedevour <luke.boyd@umontana.edu>
2017-02-28 22:38:55 -0700

    uploaded final sketch

---
+++ HW5/readme.md
@@ -0,0 +1,5 @@
+# Homework 5
+This has been the most difficult assignment that we have had thus far. The ability to "comment out" was invaluable in trying to figure out where exactly I went wrong.
+
+## Issues
+Pretty much everything gave me issues this week. I feel like I failed miserably, and will need a ton of practice on how to use variables.
--- HW5/sketch.js
+++ HW5/sketch.js
@@ -1,7 +1,15 @@
+//
 function setup() {
-
-}
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+};
 
 function draw() {
-  
-}
+  spotX = random(0, width);
+  spotY = random(0, height);
+  fill(color.r, color.g, color.b, 100);
+  ellipse(spotX, spotY, 130, 15);
+  color.r = random(100, 255);
+  color.g = random(22, 255)
+  color.b = random(55, 210);
+};
