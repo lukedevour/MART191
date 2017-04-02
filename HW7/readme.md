29df948dd1e297c8900d24d302c24177fd6cb256
lukedevour <luke.boyd@umontana.edu>
2017-04-02 11:43:19 -0600

    adding HW7

---
+++ HW7/readme.md
@@ -0,0 +1,14 @@
+# Homework 7
+This assignment had me completely stumped until I could see working examples of other projects. I can see how functions will be a powerful tool in the future.
+
+## Issues
+I can see how it might be hard to refactor your code with functions if it was not well written in the first place.
+
+## Breakthroughs
+Functions now make sense.
+
+## Results
+Eyeball monster code has been simplified and beautified.
+
+## Future work
+Functions are yet another tool in our arsenal to create simpler working code.
---
+++ HW7/sketch.js
@@ -0,0 +1,56 @@
+function setup() {
+    // Setup basics
+    createCanvas(500, 500);
+    background(0, 132, 147);
+}
+
+function draw() {
+    eyeballdude();
+}
+
+
+    function eyeballdude() {
+
+        // Body
+        push();
+        translate(250, 250);
+        strokeWeight(15);
+        ellipseMode(CENTER);
+        fill(187, 255, 19);
+        ellipse(0, 0, 200, 300);
+        pop();
+
+        // Eyeball
+        push();
+        translate(250, 250);
+        strokeWeight(15);
+        ellipseMode(CENTER);
+        fill(255, 255, 255);
+        ellipse(0, 0, 100, 150);
+        pop();
+
+        // Mouth?
+        push();
+        translate(250, 250);
+        strokeWeight(10);
+        fill(222, 29, 42);
+        triangle(-40, 160, 0, 20, 40, 160);
+        pop();
+
+        //NAME
+        push();
+        textAlign(CENTER);
+        textSize(20);
+        textFont("Serif");
+        text("Luke's Eyeball Monster", 250, 30);
+        pop();
+
+        // Pupil
+        push();
+        fill(0, 0, 0);
+        stroke(0, 255, 0);
+        ellipse(250, 250, 100, 30);
+        pop();
+
+
+    }
